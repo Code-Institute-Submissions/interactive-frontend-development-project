bd880e183418814a8f12fe920ca265c1ccd5f0f1
Ed <edmundocall.eoc@gmail.com>
2020-01-14 21:34:07 +0000

    added if else conditions for certain wages

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,22 @@ let inputBox = document.getElementById("income-input");
 let outputBox = document.getElementById("income-tax-output");
 
 function calcIncomeTaxed(){
-    outputBox.innerHTML = "Your total taxed amount for the year is " + inputBox.value * 0.20;
+    if(inputBox.value < 35000){
+        outputBox.innerHTML = "Your total taxed amount for the year is " + inputBox.value * 0.20;
+    }
+    else if(inputBox.value >= 35000){
+        let underthirtyFiveK = inputBox.value * 0.20;
+        console.log(underthirtyFiveK);
+
+        let thirtyFiveKOrMore = inputBox.value - 34999;
+        thirtyFiveKOrMore = thirtyFiveKOrMore * 0.40;
+        console.log(thirtyFiveKOrMore);
+        
+        let totalTax = underthirtyFiveK + thirtyFiveKOrMore;
+        
+        outputBox.innerHTML ="Your total taxed amount for the year is " + totalTax; 
+    }
+    
 }
 
 incomeTaxed.addEventListener("click", function(){
