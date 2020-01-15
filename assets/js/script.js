6c649bced3de045ddc6fdcb0d3d30d5f482a95ef
Ed <edmundocall.eoc@gmail.com>
2020-01-15 11:27:38 +0000

    added extra logic to under 35k & over 13k and to over 35k. added required selection dropdown logic and linked up to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,44 @@
 var totalTax;
 var underthirtyFiveK;
+var overTwelveK;
 var incomeTaxed = document.getElementById("income-taxed-btn");
 var inputBox = document.getElementById("income-input"); 
 var outputBox = document.getElementById("income-tax-output");
-
+var optionSelection = document.getElementById("blank");
+var dropDownSelection = document.getElementById("drop-down");
 
 function calcIncomeTaxed(){
-    if(inputBox.value <= 13000){
-        let overTwelveK = inputBox.value - 12012;
+    if(inputBox.value <= 13000 && dropDownSelection.value != optionSelection.value){
+        overTwelveK = inputBox.value - 12012;
         overTwelveK = overTwelveK * 0.02;
 
         underTwelveK = 12012 * 0.005;
         totalTax = overTwelveK + underTwelveK;
         totalTax = totalTax.toFixed(2);
-        outputBox.innerHTML ="Your total taxed amount for the year is €" + totalTax; 
+        outputBox.innerHTML = "Your total taxed amount for the year is €" + totalTax; 
     }
-    else if(inputBox.value < 35000 && inputBox.value > 13000){
+    else if(inputBox.value < 35000 && inputBox.value > 13000  && dropDownSelection.value != optionSelection.value){
+        overTwelveK = inputBox.value - 12012;
+        overTwelveK = overTwelveK * 0.02;
+        underTwelveK = 12012 * 0.005;
+    
         underthirtyFiveK = inputBox.value * 0.20;
-        outputBox.innerHTML = "Your total taxed amount for the year is " + underthirtyFiveK;
+        totalTax = overTwelveK + underTwelveK + underthirtyFiveK;
+        totalTax = totalTax.toFixed(2);
+        outputBox.innerHTML = "Your total taxed amount for the year is €" + totalTax;
     }
-    else if(inputBox.value >= 35000){
-        underthirtyFiveK = inputBox.value * 0.20;
-       
+    else if(inputBox.value >= 35000  && dropDownSelection.value != optionSelection.value){
+        overTwelveK = inputBox.value - 12012;
+        overTwelveK = overTwelveK * 0.02;
+        underTwelveK = 12012 * 0.005;
 
+        underthirtyFiveK = 34999 * 0.20;
         let overthirtyFiveK = inputBox.value - 34999;
         overthirtyFiveK = overthirtyFiveK * 0.40;
         
-        totalTax = underthirtyFiveK + overthirtyFiveK;
+        totalTax = overTwelveK + underTwelveK + underthirtyFiveK + overthirtyFiveK;
         totalTax = totalTax.toFixed(2);
-        outputBox.innerHTML ="Your total taxed amount for the year is €" + totalTax; 
+        outputBox.innerHTML = "Your total taxed amount for the year is €" + totalTax; 
     }
     
 }
