56783a03a51598d0d954ab65dcac0ec55af526fe
Ed <edmundocall.eoc@gmail.com>
2020-01-15 09:44:18 +0000

    added if statment for incomes under 13000 and added '&& over 13k' to under 35k else if set global variables var

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,34 @@
-let incomeTaxed = document.getElementById("income-taxed-btn");
-let inputBox = document.getElementById("income-input"); 
-let outputBox = document.getElementById("income-tax-output");
+var totalTax;
+var underthirtyFiveK;
+var incomeTaxed = document.getElementById("income-taxed-btn");
+var inputBox = document.getElementById("income-input"); 
+var outputBox = document.getElementById("income-tax-output");
+
 
 function calcIncomeTaxed(){
-    if(inputBox.value < 35000){
-        outputBox.innerHTML = "Your total taxed amount for the year is " + inputBox.value * 0.20;
+    if(inputBox.value <= 13000){
+        let overTwelveK = inputBox.value - 12012;
+        overTwelveK = overTwelveK * 0.02;
+
+        underTwelveK = 12012 * 0.005;
+        totalTax = overTwelveK + underTwelveK;
+        totalTax = totalTax.toFixed(2);
+        outputBox.innerHTML ="Your total taxed amount for the year is €" + totalTax; 
+    }
+    else if(inputBox.value < 35000 && inputBox.value > 13000){
+        underthirtyFiveK = inputBox.value * 0.20;
+        outputBox.innerHTML = "Your total taxed amount for the year is " + underthirtyFiveK;
     }
     else if(inputBox.value >= 35000){
-        let underthirtyFiveK = inputBox.value * 0.20;
-        console.log(underthirtyFiveK);
+        underthirtyFiveK = inputBox.value * 0.20;
+       
 
-        let thirtyFiveKOrMore = inputBox.value - 34999;
-        thirtyFiveKOrMore = thirtyFiveKOrMore * 0.40;
-        console.log(thirtyFiveKOrMore);
+        let overthirtyFiveK = inputBox.value - 34999;
+        overthirtyFiveK = overthirtyFiveK * 0.40;
         
-        let totalTax = underthirtyFiveK + thirtyFiveKOrMore;
-        
-        outputBox.innerHTML ="Your total taxed amount for the year is " + totalTax; 
+        totalTax = underthirtyFiveK + overthirtyFiveK;
+        totalTax = totalTax.toFixed(2);
+        outputBox.innerHTML ="Your total taxed amount for the year is €" + totalTax; 
     }
     
 }
