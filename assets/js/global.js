c6f7ccecc7b4523fd17436601937550aa9fd438c
Ed <edmundocall.eoc@gmail.com>
2020-01-22 15:23:31 +0000

    refactor, added global.js file: added jquery to add classes to buttons dropdown arrows

---
+++ assets/js/global.js
@@ -0,0 +1,14 @@
+//income tax clac button
+$(".tab-decoration").click(function(){
+    $(".dropdown-arrow-income").toggleClass("fa-chevron-circle-up").toggleClass("fa-chevron-circle-down");
+});
+
+//mpg calc button
+$(".tab-decoration2").click(function(){
+    $(".dropdown-arrow-mpg").toggleClass("fa-chevron-circle-up").toggleClass("fa-chevron-circle-down");
+});
+
+//navbar button
+$(".navbar-color-button").click(function(){
+    $(".menu-button").toggleClass("fa-caret-up").toggleClass("fa-caret-down");
+});
