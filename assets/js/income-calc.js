1f22b4c917ec36065c1932be7a1429cffdb8453e
Ed <edmundocall.eoc@gmail.com>
2020-01-25 18:37:26 +0000

    refactor of const, structure of code, variables in income-calc

--- assets/js/income-calc.js
+++ assets/js/income-calc.js
@@ -3,15 +3,15 @@ var normalUniversalSocialCharge;
 var lowerUniversalSocialCharge;
 var uSC = normalUniversalSocialCharge + lowerUniversalSocialCharge;
 var incomeTaxed = document.getElementById("income-taxed-btn");
-var inputBox = document.getElementById("income-input"); 
+var inputBox = document.getElementById("income-input");
 var outputBox = document.getElementById("income-tax-output");
 var netPayOutputBox = document.getElementById("net-pay");
 var optionSelectionEmpty = document.getElementById("blank");
 var dropDownSelection = document.getElementById("drop-down");
 
 // This calculates the rate of pay per week after tax.
-function weeklyPay(totalTax){
-    if(dropDownSelection.value !== optionSelectionEmpty.value){
+function weeklyPay(totalTax) {
+    if (dropDownSelection.value !== optionSelectionEmpty.value) {
         const WEEKS_PER_YEAR = 52;
         const WEEKLY_PAY_TEXT = "Your weekly pay for the year is: € ";
         let usersIncome = parseFloat(inputBox.value.replace(",", ""));
@@ -23,27 +23,27 @@ function weeklyPay(totalTax){
 }
 
 //This function calculates the net pay after tax's owed. 
-function netPay(totalTax){
+function netPay(totalTax) {
     let usersIncome = parseFloat(inputBox.value.replace(",", ""));
-        if(dropDownSelection.value != optionSelectionEmpty.value){
+
+    if (dropDownSelection.value != optionSelectionEmpty.value) {
         const NET_PAYTEXT = "Your net pay for the year is: € ";
-        let netPay = usersIncome - totalTax;
-        netPay = netPay.toFixed(2);
-        netPayOutputBox.innerHTML = NET_PAYTEXT + netPay; 
+        const NET_PAY = usersIncome - totalTax;
+        netPayOutputBox.innerHTML = NET_PAYTEXT + NET_PAY.toFixed(2);
     }
 }
 
 // This takes tax credits from your total tax owed, if applicable. Those under 13,000 euro are exempt from tax, thus not applicable.
-function taxCredits(){
+function taxCredits() {
     let taxCreditValue;
 
-    if(dropDownSelection.value == "single" || dropDownSelection.value == "civil-partner"){
-         //taxCreditValue = 3300;
-         const SINGLE_OR_CIVILP_TAXCREDITVALUE = 3300; 
-         taxCreditValue = SINGLE_OR_CIVILP_TAXCREDITVALUE;
+    if (dropDownSelection.value == "single" || dropDownSelection.value == "civil-partner") {
+        //taxCreditValue = 3300;
+        const SINGLE_OR_CIVILP_TAXCREDITVALUE = 3300;
+        taxCreditValue = SINGLE_OR_CIVILP_TAXCREDITVALUE;
     }
-    else if(dropDownSelection.value == "married"){
-         //taxCreditValue = 4950;
+    else if (dropDownSelection.value == "married") {
+        //taxCreditValue = 4950;
         const MARRIED_TAXCREDITVALUE = 4950;
         taxCreditValue = MARRIED_TAXCREDITVALUE;
     }
@@ -51,18 +51,14 @@ function taxCredits(){
 }
 
 
-function lowerUscRange(){
-   return lowerUniversalSocialCharge = usersIncome * LOWER_USC_RATE;
+function lowerUscRange() {
+    return lowerUniversalSocialCharge = usersIncome * LOWER_USC_RATE;
 }
 
 
 // This calculates rate of tax & usc depending on your income from 12,012 to 35,000 or more, those under 13,000 are exempt from tax, but not usc.
-function calcIncomeTaxed(){
-    let usersIncome = parseFloat(inputBox.value.replace(",", ""));
-    let totalTax = 0;
-    let normalTaxRate;
-    let higherTaxRange;
-
+function calcIncomeTaxed() {
+    const usersIncome = parseFloat(inputBox.value.replace(",", ""));
     const LOWER_USC_TAXRANGE = 12012;
     const LOWER_USC_RATE = 0.005;
     const NORMAL_USC_TAXRANGE = 13000;
@@ -71,72 +67,76 @@ function calcIncomeTaxed(){
     const NORMAL_TAX_RATE = 0.20;
     const HIGHER_TAX_RANGE = 35000;
     const HIGHER_TAX_RATE = 0.40;
+    let totalTax = 0;
+    let normalTaxRate;
+    let higherTaxRange;
 
-    if(usersIncome <= LOWER_USC_TAXRANGE){
+    if (usersIncome <= LOWER_USC_TAXRANGE) {
         lowerUniversalSocialCharge = usersIncome * LOWER_USC_RATE;
         totalTax = lowerUniversalSocialCharge;
-        outputBox.innerHTML = TAX_PAYED_OUTPUT_TEXT + totalTax.toFixed(2); 
+        outputBox.innerHTML = TAX_PAYED_OUTPUT_TEXT + totalTax.toFixed(2);
     }
-    else if(usersIncome <= NORMAL_USC_TAXRANGE){
+    else if (usersIncome <= NORMAL_USC_TAXRANGE) {
         normalUniversalSocialCharge = usersIncome - NORMAL_USC_TAXRANGE;
         normalUniversalSocialCharge = normalUniversalSocialCharge * NORMAL_USC_RATE;
 
         lowerUniversalSocialCharge = LOWER_USC_TAXRANGE * LOWER_USC_RATE;
         totalTax = normalUniversalSocialCharge + lowerUniversalSocialCharge;
-        outputBox.innerHTML = TAX_PAYED_OUTPUT_TEXT + totalTax.toFixed(2); 
+        outputBox.innerHTML = TAX_PAYED_OUTPUT_TEXT + totalTax.toFixed(2);
     }
-    else if(usersIncome <= NORMAL_TAX_RANGE && usersIncome > NORMAL_USC_TAXRANGE){
+    else if (usersIncome <= NORMAL_TAX_RANGE && usersIncome > NORMAL_USC_TAXRANGE) {
         normalUniversalSocialCharge = usersIncome - LOWER_USC_TAXRANGE;
         normalUniversalSocialCharge = normalUniversalSocialCharge * NORMAL_USC_RATE;
         lowerUniversalSocialCharge = LOWER_USC_TAXRANGE * LOWER_USC_RATE;
-        
+
         normalTaxRate = usersIncome * NORMAL_TAX_RATE;
         totalTax = normalUniversalSocialCharge + lowerUniversalSocialCharge + normalTaxRate;
-        
+
         uSC = normalUniversalSocialCharge + lowerUniversalSocialCharge;
         totalTax -= taxCredits();
         totalTax += uSC;
-        if(totalTax < 0){
+        if (totalTax < 0) {
             outputBox.innerHTML = TAX_PAYED_OUTPUT_TEXT + uSC;
         }
-        else{
+        else {
             outputBox.innerHTML = TAX_PAYED_OUTPUT_TEXT + totalTax.toFixed(2);
         }
     }
-    else if(usersIncome > NORMAL_TAX_RANGE){
+    else if (usersIncome > NORMAL_TAX_RANGE) {
         normalUniversalSocialCharge = usersIncome - LOWER_USC_TAXRANGE;
         normalUniversalSocialCharge = normalUniversalSocialCharge * NORMAL_USC_RATE;
         lowerUniversalSocialCharge = LOWER_USC_TAXRANGE * LOWER_USC_RATE;
-        
+
         normalTaxRate = NORMAL_TAX_RANGE * NORMAL_TAX_RATE;
         higherTaxRange = usersIncome - NORMAL_TAX_RANGE;
         higherTaxRange = higherTaxRange * HIGHER_TAX_RATE;
-        
+
         totalTax = normalUniversalSocialCharge + lowerUniversalSocialCharge + normalTaxRate + higherTaxRange;
-        
+
         uSC = normalUniversalSocialCharge + lowerUniversalSocialCharge;
         totalTax -= taxCredits();
         totalTax += uSC;
-        if(totalTax < 0){
+        if (totalTax < 0) {
             outputBox.innerHTML = TAX_PAYED_OUTPUT_TEXT + uSC;
         }
-        else{
+        else {
             outputBox.innerHTML = TAX_PAYED_OUTPUT_TEXT + totalTax.toFixed(2);
         }
     }
     return totalTax;
 }
-// button listener
-incomeTaxed.addEventListener("click", function(){
 
-            let usersIncome = parseFloat(inputBox.value.replace(",", ""));
-            if (isNaN(usersIncome) && dropDownSelection.value != optionSelectionEmpty.value) {
-                outputBox.innerHTML = "Invalid Input... Try numbers only";
-            }
-            else if(dropDownSelection.value != optionSelectionEmpty.value){
-                // setting a variable to be the returned value of the function that it is equals to.
-                let taxValueReturned = calcIncomeTaxed();
-                netPay(taxValueReturned);
-                weeklyPay(taxValueReturned);
-            }
+// button listener
+incomeTaxed.addEventListener("click", function () {
+
+    const usersIncome = parseFloat(inputBox.value.replace(",", ""));
+    if (isNaN(usersIncome) && dropDownSelection.value != optionSelectionEmpty.value) {
+        outputBox.innerHTML = "Invalid Input... Try numbers only";
+    } 
+    else if (dropDownSelection.value != optionSelectionEmpty.value) {
+        // setting a variable to be the returned value of the function that it is equals to.
+        const taxValueReturned = calcIncomeTaxed();
+        netPay(taxValueReturned);
+        weeklyPay(taxValueReturned);
+    }
 }); 
--- assets/js/mpg-calc.js
+++ assets/js/mpg-calc.js
@@ -1,7 +1,7 @@
-var inputDistance = document.getElementById("miles-or-kms-traveled");    
+var inputDistance = document.getElementById("miles-or-kms-traveled");
 var inputLitres = document.getElementById("litres-input");
 
-function calcMpg(outputTextValue){
+function calcMpg(outputTextValue) {
     let outputValue = document.getElementById("mpg-output");
     let outputText = document.getElementById("mpg-text");
     let radioBtnUK = document.getElementsByClassName("uk-mpg");
@@ -12,12 +12,12 @@ function calcMpg(outputTextValue){
     let mpg = 0;
 
     mpg = inputDistance.value / inputLitres.value;
-    
-    if(ukCheckedRadioBtn.value === ""){
+
+    if (ukCheckedRadioBtn.value === "") {
         const UK_GALLON = 4.25;
         gallonRate = UK_GALLON;
     }
-    else if(usCheckedRadioBtn.value === ""){
+    else if (usCheckedRadioBtn.value === "") {
         const US_GALLON = 3.75;
         gallonRate = US_GALLON;
     }
@@ -26,13 +26,13 @@ function calcMpg(outputTextValue){
     outputText.innerHTML = outputTextValue;
 }
 
-function setOutputTextValue(){
+function setOutputTextValue() {
     let outputTextValue;
-    if($("#kms-option").val() === "selected"){
+    if ($("#kms-option").val() === "selected") {
         const KM_OUTPUT_TEXT = "Kilometers";
         outputTextValue = KM_OUTPUT_TEXT;
     }
-    else if($("#miles-option").val() === "selected"){
+    else if ($("#miles-option").val() === "selected") {
         const MILES_OUTPUT_TEXT = "Miles";
         outputTextValue = MILES_OUTPUT_TEXT;
     }
@@ -40,34 +40,34 @@ function setOutputTextValue(){
 }
 
 //On click function
-$("#calc-mpg-btn").click(function() {
-    if(isNaN(inputDistance.value) || isNaN(inputLitres.value) || $("#drop-down1").val() == ""){
+$("#calc-mpg-btn").click(function () {
+    if (isNaN(inputDistance.value) || isNaN(inputLitres.value) || $("#drop-down1").val() == "") {
         $("#mpg-output").text("Invalid..");
     }
-    else{
+    else {
         let setMilesOrKmsText = setOutputTextValue();
         calcMpg(setMilesOrKmsText);
     }
 });
 
 //Dropdown option selected set value to selected.
-$("#kms-option").click(function(){
+$("#kms-option").click(function () {
     $(this).val("selected");
     $("#miles-option").val("");
 });
 
-$("#miles-option").click(function(){
+$("#miles-option").click(function () {
     $(this).val("selected");
     $("#kms-option").val("");
 });
 
 //Radio button selected set value to checked.
-$("#option2").click(function(){
+$("#option2").click(function () {
     $(this).val("checked");
     $("#option1").val("");
 });
 
-$("#option1").click(function(){
+$("#option1").click(function () {
     $(this).val("checked");
     $("#option2").val("");
 });
