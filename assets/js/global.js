de4388bf7162183aeaf5e8dd634898e5ac8b36eb
Ed <edmundocall.eoc@gmail.com>
2020-01-28 15:00:29 +0000

    added comments to global.js and added dropdown arrow functions

--- assets/js/global.js
+++ assets/js/global.js
@@ -3,31 +3,43 @@ $(".navbar-color-button").click(function(){
     $(".menu-button").toggleClass("fa-caret-up").toggleClass("fa-caret-down");
 });
 
-//income tax clac button
+//income tax clac change arrow icon and collapse content.
 $(".tab-decoration").click(function(){
     const mpgBtn = $("#mpg-calc");
     const mpgContent = $("#collapseTwo");
 
-    $(".dropdown-arrow-income").toggleClass("fa-chevron-circle-up").toggleClass("fa-chevron-circle-down");
+    incomeTaxToggleArrow();
 
     if ($("#income-tax").attr('aria-expanded')) {
             $("#mpg-calc").attr('aria-expanded', 'true');
             mpgBtn.removeClass("collapsed");
             mpgContent.addClass("show");
+            mpgCalcToggleArrow();
     }
 });
 
-//mpg calc button
+function incomeTaxToggleArrow(){
+
+    $(".dropdown-arrow-income").toggleClass("fa-chevron-circle-up").toggleClass("fa-chevron-circle-down");
+
+}
+
+function mpgCalcToggleArrow(){
+    $(".dropdown-arrow-mpg").toggleClass("fa-chevron-circle-up").toggleClass("fa-chevron-circle-down");
+}
+
+//mpg calc change arrow icon and collapse content.
 $(".tab-decoration2").click(function(){
     const incomeTaxBtn = $("#income-tax");
     const incomeTaxContent = $("#collapseOne");
 
-    $(".dropdown-arrow-mpg").toggleClass("fa-chevron-circle-up").toggleClass("fa-chevron-circle-down");
+    mpgCalcToggleArrow();
 
      if ($("#mpg-calc").attr('aria-expanded')) {
             $("#income-tax").attr('aria-expanded', 'true');
             incomeTaxBtn.removeClass("collapsed");
             incomeTaxContent.addClass("show");
+            incomeTaxToggleArrow();
     }
     
 });
