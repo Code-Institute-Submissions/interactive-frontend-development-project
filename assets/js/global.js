b79d5f074fb03239b28fb8d613baf422d4b3fb2c
Ed <edmundocall.eoc@gmail.com>
2020-01-28 12:39:37 +0000

    added if statement for dropdown content apps to keep one open at all times

--- assets/js/global.js
+++ assets/js/global.js
@@ -5,11 +5,31 @@ $(".navbar-color-button").click(function(){
 
 //income tax clac button
 $(".tab-decoration").click(function(){
+    const mpgBtn = $("#mpg-calc");
+    const mpgContent = $("#collapseTwo");
+
     $(".dropdown-arrow-income").toggleClass("fa-chevron-circle-up").toggleClass("fa-chevron-circle-down");
+
+    if ($("#income-tax").attr('aria-expanded')) {
+            $("#mpg-calc").attr('aria-expanded', 'true');
+            mpgBtn.removeClass("collapsed");
+            mpgContent.addClass("show");
+    }
 });
 
 //mpg calc button
 $(".tab-decoration2").click(function(){
+    const incomeTaxBtn = $("#income-tax");
+    const incomeTaxContent = $("#collapseOne");
+
     $(".dropdown-arrow-mpg").toggleClass("fa-chevron-circle-up").toggleClass("fa-chevron-circle-down");
+
+     if ($("#mpg-calc").attr('aria-expanded')) {
+            $("#income-tax").attr('aria-expanded', 'true');
+            incomeTaxBtn.removeClass("collapsed");
+            incomeTaxContent.addClass("show");
+    }
+    
 });
+
 
