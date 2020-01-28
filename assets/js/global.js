e7dea16203ae4c865be820990d50e54965e7dbf0
Ed <edmundocall.eoc@gmail.com>
2020-01-28 10:26:32 +0000

    added styling to navbar elements

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,3 +1,8 @@
+//navbar button
+$(".navbar-color-button").click(function(){
+    $(".menu-button").toggleClass("fa-caret-up").toggleClass("fa-caret-down");
+});
+
 //income tax clac button
 $(".tab-decoration").click(function(){
     $(".dropdown-arrow-income").toggleClass("fa-chevron-circle-up").toggleClass("fa-chevron-circle-down");
@@ -8,7 +13,3 @@ $(".tab-decoration2").click(function(){
     $(".dropdown-arrow-mpg").toggleClass("fa-chevron-circle-up").toggleClass("fa-chevron-circle-down");
 });
 
-//navbar button
-$(".navbar-color-button").click(function(){
-    $(".menu-button").toggleClass("fa-caret-up").toggleClass("fa-caret-down");
-});
