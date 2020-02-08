f32518374c1a28c3873f36e312341a5d89621cf4
Ed <edmundocall.eoc@gmail.com>
2020-02-08 12:55:35 +0000

    removed toggle chevron arrows for web apps buttons drop down content, as it is no longer needed

--- assets/js/global.js
+++ assets/js/global.js
@@ -5,58 +5,20 @@ $(".navbar-color-button").click(function() {
 
 });
 
-//toggle arrow on click of income tax button
-function incomeTaxToggleArrow() {
-
-    if ($(".dropdown-arrow-income").hasClass("fa-chevron-circle-up")) {
-
-        $(".dropdown-arrow-income").removeClass("fa-chevron-circle-up");
-        $(".dropdown-arrow-income").addClass("fa-chevron-circle-down");
-
-    }
-    else if($(".dropdown-arrow-income").hasClass("fa-chevron-circle-down")) {
-
-        $(".dropdown-arrow-income").removeClass("fa-chevron-circle-down");
-        $(".dropdown-arrow-income").addClass("fa-chevron-circle-up");
-
-    }
-
-}
-
-//toggle arrow on click of mpg button
-function mpgCalcToggleArrow() {
-
-    if ($(".dropdown-arrow-mpg").hasClass("fa-chevron-circle-up")) {
-
-        $(".dropdown-arrow-mpg").removeClass("fa-chevron-circle-up");
-        $(".dropdown-arrow-mpg").addClass("fa-chevron-circle-down");
-
-    }
-    else if ($(".dropdown-arrow-mpg").hasClass("fa-chevron-circle-down")) {
-
-        $(".dropdown-arrow-mpg").removeClass("fa-chevron-circle-down");
-        $(".dropdown-arrow-mpg").addClass("fa-chevron-circle-up");
-
-    }
-
-}
-
-//income tax clac change arrow icon and collapse content.
+//income tax clac collapse content.
 $(".tab-decoration").click(function() {
 
     let mpgContent = $("#collapseTwo");
 
-    incomeTaxToggleArrow();
     toggleCalcContent($("#mpg-calc"), mpgContent);
 
 });
 
-//mpg calc change arrow icon and collapse content.
+//mpg calc change collapse content.
 $(".tab-decoration2").click(function() {
 
     let incomeTaxContent = $("#collapseOne");
 
-    mpgCalcToggleArrow();
     toggleCalcContent($("#income-tax"), incomeTaxContent);
     keepMpgOpen();
 
@@ -89,14 +51,14 @@ function toggleCalcContent(calc, content) {
         }
 
     }
-    if ((calc.attr('aria-expanded') == "true" && otherCalc.attr('aria-expanded') == "true")) {
+    if (calc.attr('aria-expanded') == "true" && otherCalc.attr('aria-expanded') == "true") {
 
         otherCalc.attr('aria-expanded', 'false');
         otherCalc.addClass("collapsed");
         content.removeClass("show");
 
     }
-    else if ((calc.attr('aria-expanded') == "false" && otherCalc.attr('aria-expanded') == "false")) {
+    else if (calc.attr('aria-expanded') == "false" && otherCalc.attr('aria-expanded') == "false") {
 
         otherCalc.attr('aria-expanded', 'true');
         otherCalc.removeClass("collapsed");
@@ -106,10 +68,10 @@ function toggleCalcContent(calc, content) {
 
 }
 
-// Keeps mpg open if both are closed.
+// Keeps mpg open.
 function keepMpgOpen() {
 
-    if (($("#mpg-calc").attr('aria-expanded') == "false" || $("#income-tax").attr('aria-expanded') == "false")) {
+    if ($("#mpg-calc").attr('aria-expanded') == "false" || $("#income-tax").attr('aria-expanded') == "false") {
 
         $("#mpg-calc").attr('aria-expanded', 'true');
         $("#mpg-calc").removeClass("collapsed");
